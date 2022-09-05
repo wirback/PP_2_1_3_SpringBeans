9d21c76d813b16e1e7de7ab78135d686cb8bab20
wirback <ladnov.dmitry@yandex.ru>
2022-09-05 20:57:32 +0300

    version_0.003 Создал бин Timer и связал с AnimalsCage.

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,20 +1,23 @@
 package app.model;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
-    @Autowired
-    @Qualifier("dog")
-    private Animal animal;
+    private final Animal animal;
+    private final Timer timer;
+
+    public AnimalsCage(@Qualifier(value = "dog") Animal animal, Timer timer) {
+        this.animal = animal;
+        this.timer = timer;
+    }
 
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(timer.getTime());
         System.out.println("________________________");
     }
 }
