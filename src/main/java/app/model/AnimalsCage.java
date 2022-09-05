fc0be1b327592f222eb9cd238867720ddcfe174c
wirback <ladnov.dmitry@yandex.ru>
2022-09-05 20:12:43 +0300

    version_0.002 Связал AnimalCage с бином Dog по имени.

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,12 +1,13 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
-
     @Autowired
+    @Qualifier("dog")
     private Animal animal;
 
     public void whatAnimalSay() {
